8ec5ffee670f5e923e05cba7442eb3582eb19d4c
Mapgie Embedded <magpieembedded@gmail.com>
2025-03-31 23:36:47 +0100

    Fix: Amend openocd path in build script template

--- scripts/build_template.sh
+++ scripts/build_template.sh
@@ -1,3 +1,3 @@
 echo "Building RPI_PICO_BOARD with command:"
 set -x
-west build -b RPI_PICO_BOARD app -p auto -- -DOPENOCD=OPENOCD_INSTALL_DIR/openocd/src/openocd -DOPENOCD_DEFAULT_PATH=OPENOCD_INSTALL_DIR/tcl
+west build -b RPI_PICO_BOARD app -p auto -- -DOPENOCD=OPENOCD_INSTALL_DIR/src/openocd -DOPENOCD_DEFAULT_PATH=OPENOCD_INSTALL_DIR/tcl
