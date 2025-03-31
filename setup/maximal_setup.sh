a0d0e0b9902e03462588d143f3c0239512eef477
Mapgie Embedded <magpieembedded@gmail.com>
2025-03-31 14:55:56 +0100

    Feat: Add pico SDK dependencies to maximal setup script

--- setup/maximal_setup.sh
+++ setup/maximal_setup.sh
@@ -2,10 +2,14 @@
 sudo apt update -y
 sudo apt upgrade -y
 
+# Zephyr dependencies
 sudo apt install -y --no-install-recommends git cmake ninja-build gperf \
   ccache dfu-util device-tree-compiler wget \
   python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
   make gcc libsdl2-dev libmagic1
+
+# Pico SDK dependencies
+sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential pkg-config
 
 # Required on other linux platforms
 # sudo apt install --no-install-recommends gcc-multilib g++-multilib
