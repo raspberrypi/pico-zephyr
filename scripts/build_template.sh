0fc053eebbb8fcee791004e574d9e10ad9d7a3e9
Mapgie Embedded <magpieembedded@gmail.com>
2025-04-01 15:48:14 +0100

    Feat: Add usb serial overlay to build template

--- scripts/build_template.sh
+++ scripts/build_template.sh
@@ -1,3 +1,3 @@
 echo "Building RPI_PICO_BOARD with command:"
 set -x
-west build -b RPI_PICO_BOARD app -p auto -- -DOPENOCD=OPENOCD_INSTALL_DIR/src/openocd -DOPENOCD_DEFAULT_PATH=OPENOCD_INSTALL_DIR/tcl
+west build -b RPI_PICO_BOARD app -p auto -- -DOPENOCD=OPENOCD_INSTALL_DIR/src/openocd -DOPENOCD_DEFAULT_PATH=OPENOCD_INSTALL_DIR/tcl -DEXTRA_CONF_FILE="usb_serial_port/usb_serial_port.conf" -DEXTRA_DTC_OVERLAY_FILE="usb_serial_port.overlay"
