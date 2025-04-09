197f42953be7cb720812f67d360b2d30b56d7d62
Mapgie Embedded <magpieembedded@gmail.com>
2025-04-09 13:09:11 +0100

    Refactor: Move ping code to separate file

--- wifi_example/src/main.c
+++ wifi_example/src/main.c
@@ -9,11 +9,12 @@
 #include <zephyr/posix/netdb.h>
 #include <zephyr/net/http/client.h>
 #include <zephyr/net/net_config.h>
-#include <zephyr/net/icmp.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_ip.h>
 #include <zephyr/net/socket.h>
 #include <zephyr/net/wifi_mgmt.h>
+
+#include "ping.h"
 
 // Helper macros
 #define CHECK(r) { if (r < 0) { printf("Error: %d\n", (int)r); exit(1); } }
@@ -41,59 +42,6 @@ void dump_addrinfo(const struct addrinfo *ai)
 	       "sa_family=%d, sin_port=%x\n",
 	       ai, ai->ai_family, ai->ai_socktype, ai->ai_protocol, ai->ai_addr->sa_family,
 	       ntohs(((struct sockaddr_in *)ai->ai_addr)->sin_port));
-}
-
-int icmp_echo_reply_handler(struct net_icmp_ctx *ctx,
-				struct net_pkt *pkt,
-				struct net_icmp_ip_hdr *hdr,
-				struct net_icmp_hdr *icmp_hdr,
-				void *user_data)
-{
-	uint32_t cycles;
-	char ipv4[INET_ADDRSTRLEN];
-	zsock_inet_ntop(AF_INET, &hdr->ipv4->src, ipv4, INET_ADDRSTRLEN);
-
-	uint32_t *start_cycles = user_data;
-
-	cycles = k_cycle_get_32() - *start_cycles;
-
-	printk("Reply from %s: bytes=%d time=%dms TTL=%d\r\n",
-			ipv4,
-			ntohs(hdr->ipv4->len),
-			((uint32_t)k_cyc_to_ns_floor64(cycles) / 1000000),
-			hdr->ipv4->ttl);
-
-	return 0;
-}
-
-void ping(char* ipv4_addr, uint8_t count)
-{
-	uint32_t cycles;
-	int ret;
-	struct net_icmp_ctx icmp_context;
-
-	// Register handler for echo reply
-	ret = net_icmp_init_ctx(&icmp_context, NET_ICMPV4_ECHO_REPLY, 0, icmp_echo_reply_handler);
-	if (ret != 0) {
-		printk("Failed to init ping, err: %d", ret);
-	}
-
-	struct net_if *iface = net_if_get_default();
-	struct sockaddr_in dst_addr;
-	net_addr_pton(AF_INET, ipv4_addr, &dst_addr.sin_addr);
-	dst_addr.sin_family = AF_INET;
-
-	for (int i = 0; i < count; i++)
-	{
-		cycles = k_cycle_get_32();
-		ret = net_icmp_send_echo_request(&icmp_context, iface, (struct sockaddr *)&dst_addr, NULL, &cycles);
-		if (ret != 0) {
-			printk("Failed to send ping, err: %d", ret);
-		}
-		k_sleep(K_SECONDS(1));
-	}
-
-	net_icmp_cleanup_ctx(&icmp_context);
 }
 
 void wifi_connect()
