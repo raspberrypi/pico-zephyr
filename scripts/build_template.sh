12aaa634cc4da21e2ed3acea0b79e610914244d2
Mapgie Embedded <magpieembedded@gmail.com>
2025-04-09 21:03:03 +0100

    Feat: Add second cli option for build script to build other applications

--- scripts/build_template.sh
+++ scripts/build_template.sh
@@ -6,8 +6,19 @@ if [ $# -eq 1 ]
         else
             SNIPPET_USB=""
     fi
+    APP="app"
+fi
+
+if [ $# -eq 2 ]
+    then if [ "$2" == "usb_serial_port" ]
+        then
+            SNIPPET_USB="-S usb_serial_port"
+        else
+            SNIPPET_USB=""
+    fi
+    APP=$1
 fi
 
 echo "Building RPI_PICO_BOARD with command:"
 set -x
-SNIPPET_ROOT="$(pwd)" west build -b RPI_PICO_BOARD app -p auto $SNIPPET_USB -- -DOPENOCD=OPENOCD_INSTALL_DIR/src/openocd -DOPENOCD_DEFAULT_PATH=OPENOCD_INSTALL_DIR/tcl
+SNIPPET_ROOT="$(pwd)" west build -b RPI_PICO_BOARD $APP -p auto $SNIPPET_USB -- -DOPENOCD=OPENOCD_INSTALL_DIR/src/openocd -DOPENOCD_DEFAULT_PATH=OPENOCD_INSTALL_DIR/tcl
